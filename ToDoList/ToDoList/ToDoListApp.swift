c708b3da24acc485b55c2d9f8277cb90ac8c3038
“Davit <2840850M@student.gla.ac.uk>
2025-06-22 16:05:41 +0100

    Initial commit

---
+++ ToDoList/ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Davit Muradyan on 22.06.25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
